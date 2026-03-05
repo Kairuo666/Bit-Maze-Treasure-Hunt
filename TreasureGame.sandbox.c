18466263e8afaea9618fd4360341c435ceb2b401
Oliver Brzezinski - STUDENT <g00475000@atu.ie>
2026-03-05 16:28:15 +0000

    code for 6x6 matrix

--- TreasureGame.sandbox.c
+++ TreasureGame.sandbox.c
@@ -1,12 +1,35 @@
 ﻿#include <stdio.h>
+#include <stdlib.h>
 #include <math.h>
+#include <time.h>
 
 
 // Treasure Game sandbox
 
 int main()
 {
+	// 6x6 grid of randoms
+    int i, j;
+	char x[36];
+    int disp[6][6];
 
+    srand(time(NULL));
+
+    for (i = 0; i < 6; i++) {
+        for (j = 0; j < 6; j++) {
+            disp[i][j] = rand() % 10;
+        }
+    }
+
+   
+    printf("The 6x6 matrix:\n\n");
+    for (i = 0; i < 6; i++) {
+        for (j = 0; j < 6; j++) {
+            printf("%d ", disp[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
 }
 
 
